e50ebff296eb6ca55f56e9152cf1ef2524479ead
shekh <shekharsushant54@gmail.com>
2024-04-02 14:18:00 +0530

    final changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
 <div Name="Home" className='flex h-screen w-full bg-gradient-to-b via-black from-black to-gray-800 '>
     <div className='max-w-screen-lg mx-auto  flex flex-col items-center justify-center h-full px-4 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
-            <h2 className='text-4xl sm:text-7xl py-10 font-bold text-white  '>I'm a Front-End Developer | Graphics Designer</h2>
+            <h2 className='text-4xl sm:text-7xl py-10 font-bold text-white  '>I'm a Full Stack Developer | Graphics Designer</h2>
             <p className='text-gray-500 py-4 max-width-md'>A highly motivated and keen learner, with an optimistic mindset. Deals with problems practically and strategically. Looking for opportunities with a constant path to evolve and contribute to the organisation's growth in the process.
                 Currently I love to work on web  application using technologies like React , Bootstrap , Tailswind , HTML5 , CSS and javaScript . 
                 I love to make UI/UX designs , logo and vector potrait using Adobe PhotoShop and Adobe Illustrator
